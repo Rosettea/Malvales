208b21813418e40f122f791dff8bd71e7216428a
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-07-06 23:15:45 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Malvales
+> 🕊 Public interfaces and functions for Hilbish native modules.
+
+This repository holds public Go interfaces and functions to facilitate
+native user modules for Hilbish. These will be used to ensure validity
+in modules and to provide utility functions for creating and exposing them.
+
+Why is it named Malvales? Because they are the order of Hibiscus, what the
+name Hilbish is roughly taken from.
+
+# License
+MIT
---
+++ plugin.go
@@ -0,0 +1,48 @@
+package malvales
+
+import (
+	"net/rpc"
+
+	"github.com/hashicorp/go-plugin"
+	rt "github.com/arnodel/golua/runtime"
+)
+
+type Plugin interface{
+	Load(*rt.Runtime) rt.Value
+}
+
+type entryRPCClient struct{
+	client *rpc.Client
+}
+
+func (e *entryRPCClient) Load(rtm *rt.Runtime) rt.Value {
+	var resp *rt.Value
+	err := e.client.Call("Plugin.Load", rtm, &resp)
+	if err != nil {
+		// TODO: return nil (or some value to indicate err)
+		panic(err)
+	}
+
+	return *resp
+}
+
+type entryRPCServer struct{
+	P *Plugin
+}
+
+func (s *entryRPCServer) Load(rtm *rt.Runtime, resp *rt.Value) error {
+	*resp = s.P.Load(rtm)
+	return nil
+}
+
+type Entry struct{
+	P *Plugin
+}
+
+func (e *Entry) Server(b *plugin.MuxBroker) (interface{}, error) {
+	return &entryRPCServer{P: e.P}, nil
+}
+
+func (e *Entry) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
+	return &entryRPCClient{client: c}, nil
+}
